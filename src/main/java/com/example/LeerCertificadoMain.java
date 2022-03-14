e320b5dc9b9d749f7de44b356ac6d62c85453a3d
23maldonadojhon <23maldonadojhon@gmail.com>
2022-03-14 10:00:57 -0500

    Actunaiozacion con BC

--- src/main/java/com/example/LeerCertificadoMain.java
+++ src/main/java/com/example/LeerCertificadoMain.java
@@ -6,6 +6,10 @@ import java.security.cert.CertificateException;
 import java.security.cert.CertificateFactory;
 import java.security.cert.X509Certificate;
 
+import org.bouncycastle.asn1.x500.RDN;
+import org.bouncycastle.asn1.x500.X500Name;
+import org.bouncycastle.asn1.x500.style.BCStyle;
+
 public class LeerCertificadoMain {
 	
 	
@@ -14,13 +18,30 @@ public class LeerCertificadoMain {
 		try {
 			
 			CertificateFactory certificate = CertificateFactory.getInstance("X509");
-			FileInputStream is = new FileInputStream("C:\\home\\AVALON\\CERTIFICADOS\\CERTIFICADOS_PRUEBAS\\ciudadano_ficticio.cer");
-			X509Certificate cert = (X509Certificate) certificate.generateCertificate(is);
+			FileInputStream is = new FileInputStream("C:/home/AVALON/CERTIFICADOS/CERTIFICADOS_PRUEBAS/ciudadano_ficticio.cer");
+			X509Certificate x509Certificate = (X509Certificate) certificate.generateCertificate(is);
 			
-			System.out.print("CERTIFICADO TO STRING ="+cert.toString());
-			System.out.print("CERTIFICADO BYTE ="+cert.getEncoded());
-						
-
+		
+			System.out.println("================================================================================");
+			System.out.println("CERTIFICADO TO STRING ="+x509Certificate.toString());
+			System.out.println("");
+			
+			System.out.println("EMISOR==========================================================================");			
+			System.out.println(x509Certificate.getIssuerDN());
+			System.out.println("");
+			
+			System.out.println("SUJETO=========================================================================");
+			System.out.println(x509Certificate.getSubjectDN());
+			System.out.println("");
+			
+			System.out.println("LEER DATOS DEL CETIFICADO========================================================");
+			X500Name x500Name = new X500Name( x509Certificate.getSubjectX500Principal().getName());
+			RDN cn = x500Name.getRDNs(BCStyle.CN)[0];	
+			RDN serialNumber = x500Name.getRDNs(BCStyle.SERIALNUMBER)[0];	
+			
+			System.out.println("CN="+cn.getFirst().getValue());
+			System.out.println("SERIALNUMBER="+serialNumber.getFirst().getValue());
+			
 		} catch (CertificateException e) {
 			e.printStackTrace();
 		} catch (FileNotFoundException e) {
